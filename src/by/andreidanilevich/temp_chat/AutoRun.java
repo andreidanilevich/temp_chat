b9c38a23e442d3002e681bc1c0394783393966d7
andreidanilevich <andreidanilevich@gmail.com>
2015-10-06 22:56:27 +0200

    рабочий после проверки

--- src/by/andreidanilevich/temp_chat/AutoRun.java
+++ src/by/andreidanilevich/temp_chat/AutoRun.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class AutoRun extends BroadcastReceiver { // автостарт
+public class AutoRun extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
